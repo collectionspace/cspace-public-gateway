465b2048799748b71ed9ee66fe358812510d267d
Ray Lee <ray.lee@lyrasis.org>
2024-03-14 00:00:52 -0400

    Update dashboard.
    Show all tenant-qualified properties, and links to system info and ES count pages for each tenant.

--- src/main/java/org/collectionspace/publicbrowser/web/DashboardController.java
+++ src/main/java/org/collectionspace/publicbrowser/web/DashboardController.java
@@ -1,10 +1,20 @@
 package org.collectionspace.publicbrowser.web;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.EnumerablePropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.core.env.PropertySource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,28 +24,46 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class DashboardController {
     @Autowired
     private Environment environment;
-    private String properties[] = new String[] {
-        "es.allowedPublishToValues",
-        "es.allowedRecordTypes",
-        "es.recordTypes.CollectionObject.publishToField",
-        "es.recordTypes.Media.publishToField",
-        "zuul.routes.core-cspace-services.url",
-        "zuul.routes.core-cspace-services.username",
-        "zuul.routes.core-es.url",
-        "zuul.routes.core-es.allowedPublishToValues",
-        "zuul.routes.core-es.allowedRecordTypes",
-        "zuul.routes.core-es.recordTypes.CollectionObject.publishToField",
-        "zuul.routes.core-es.recordTypes.Media.publishToField",
-    };
+    private List<String> configurationPropertyPrefixes = Arrays.asList("es", "zuul");
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String dashboard(Model model) {
-        Map<String, String> config = new LinkedHashMap<>();
-        for(String property : properties) {
-            config.put(property, environment.getProperty(property));
+        model.addAttribute("config", getConfigurationProperties());
+
+        return "dashboard";
+    }
+
+    private Map<String, String> getConfigurationProperties() {
+        if (!(environment instanceof ConfigurableEnvironment)) {
+            return new HashMap<>();
         }
 
-        model.addAttribute("config", config);
-        return "dashboard";
+        ConfigurableEnvironment configurableEnvironment = (ConfigurableEnvironment) environment;
+
+        return StreamSupport.stream(configurableEnvironment.getPropertySources().spliterator(), false)
+            .flatMap(propertySource -> getConfigurationPropertyNames(propertySource))
+            .distinct()
+            .sorted()
+            .collect(Collectors.toMap(
+                Function.identity(),
+                propertyName -> environment.getProperty(propertyName),
+                (existing, replacement) -> existing,
+                LinkedHashMap::new)
+            );
+    }
+
+    private Stream<String> getConfigurationPropertyNames(PropertySource<?> propertySource) {
+        if (!(propertySource instanceof EnumerablePropertySource)) {
+            return Stream.of();
+        }
+
+        return Stream.of(((EnumerablePropertySource<?>) propertySource).getPropertyNames())
+            .filter(propertyName -> isConfigurationProperty(propertyName))
+            .filter(propertyName -> !propertyName.contains("password"));
+    }
+
+    private boolean isConfigurationProperty(String propertyName) {
+        return configurationPropertyPrefixes.stream()
+            .anyMatch(prefix -> propertyName.startsWith(prefix));
     }
 }
